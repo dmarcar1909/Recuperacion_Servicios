11dc941ae0a424024f7437a44a04d8f3f482abc9
dmarcar1909 <117440310+dmarcar1909@users.noreply.github.com>
2025-05-31 23:46:50 +0200

    Update main.tf
    Bucket s3 para el tfstate

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region = var.aws_region
 }
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-estado-damian"
+    key    = "infraestructura/terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
+  }
+}
